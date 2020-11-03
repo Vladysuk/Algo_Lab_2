29a811309052f1c618c0a2c7cc60a74c282c6ab1
Vlad Dorofeiev <vladik1543u7@gmail.com>
2020-11-03 13:27:52 +0200

    changed default sort to my own

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from algorithm import algorithm
+from quick_sort import quick_sort
 
 if __name__ == '__main__':
     N = 3
@@ -6,7 +7,7 @@ if __name__ == '__main__':
         prices = [int(x) for x in next(fileIn).split()]
         discount = int(fileIn.readline())
 
-    prices.sort()
+    quick_sort(prices, 0, len(prices)-1)
     total_price = algorithm(prices, discount, N)
 
     with open('discnt.out', 'w') as fileOut:
